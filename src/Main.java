044cd21b022e201568205a1f140416a10542cc36
Farhan249-ctrl <farhanshaik44000@gmail.com>
2026-05-03 19:20:03 +0530

    Prime Numbers

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc=new Scanner(System.in);
+        boolean isPrime=true;
+        int n =sc.nextInt();
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+               isPrime=false;
+               break;
+            }
 
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        }
+        if(isPrime){
+            System.out.println("n is prime");
+        }
+        else{
+            System.out.println("n is not prime");
         }
     }
 }
